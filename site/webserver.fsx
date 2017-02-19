86ad07e5abebc2a7515f02f328cc1cd437008a84
Marnee <marnee.dearman@gmail.com>
2017-02-19 13:17:14 -0700

    Upgraded to Suave 2.0 and Fsharp 4.0 and FsPickler 3.2

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -7,18 +7,14 @@
 
 open Fake
 open Suave
-open Suave.Http.Successful
-open Suave.Web
-open Suave.Types
 open System.Net
-
 let serverConfig = 
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
-    { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
+    { defaultConfig with bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
 
 startWebServer serverConfig 
-    (OK  
-        ("Hello World! It's Marnee Dearman Suave.io on Azure Websites.<br /><ul>" + 
+    (Successful.OK  
+        ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" + 
           "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
           "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
           "</ul>"))
